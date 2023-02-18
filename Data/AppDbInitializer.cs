e576e997fc301307b81445ae4ad5270d4cadcc5e
sed-ID <sadid.islam@g.bracu.ac.bd>
2023-02-18 15:46:52 +0600

    Add project files.

---
+++ Data/AppDbInitializer.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using shelter.Models;
+
+namespace shelter.Data
+{
+    public class AppDbInitializer
+    {
+        public class AppDbInitializer
+        {
+            public static void Seed(IApplicationBuilder applicationBuilder)
+            {
+                using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+                {
+                    var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                    context.Database.EnsureCreated();
+
+                    { }
+                        
+        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+            {
+                using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+                {
+
+                    //Roles
+                    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+                        await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+                    if (!await roleManager.RoleExistsAsync(UserRoles.User))
+                        await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+                    //Users
+                    var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                    string adminUserEmail = "admin@etickets.com";
+
+                    var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
+                    if (adminUser == null)
+                    {
+                        var newAdminUser = new ApplicationUser()
+                        {
+                            FullName = "Admin User",
+                            UserName = "admin-user",
+                            Email = adminUserEmail,
+                            EmailConfirmed = true
+                        };
+                        await userManager.CreateAsync(newAdminUser, "Coding@1234?");
+                        await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
+                    }
+
+
+                    string appUserEmail = "user@etickets.com";
+
+                    var appUser = await userManager.FindByEmailAsync(appUserEmail);
+                    if (appUser == null)
+                    {
+                        var newAppUser = new ApplicationUser()
+                        {
+                            FullName = "Application User",
+                            UserName = "app-user",
+                            Email = appUserEmail,
+                            EmailConfirmed = true
+                        };
+                        await userManager.CreateAsync(newAppUser, "Coding@1234?");
+                        await userManager.AddToRoleAsync(newAppUser, UserRoles.User);
+                    }
+                }
+            }
+        }
+    }
---
+++ Models/Org.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace shelter.Models
+{
+    public class Org
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string ProfilePictureName { get; set;}
+        public string Description { get; set;}
+
+    }
+}
